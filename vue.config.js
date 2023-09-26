16ec1cebc365c003e72b112ce3b20fc1b3455ca3
Dasha <dasha326@yandex.ru>
2023-09-26 17:45:16 +0300

    fix config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: '/test-vue-wysiwyg/',
   css: {
     loaderOptions: {
       scss: {
